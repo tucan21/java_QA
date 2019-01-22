076fc27633571c88bb0003c06324eafda905df21
tucan21 <stat21@interia.pl>
2019-01-22 16:36:36 +0100

    Configurtion changes

--- sandbox/src/main/java/pl/java_qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/java_qa/sandbox/MyFirstProgram.java
@@ -1,10 +1,10 @@
 package pl.java_qa.sandbox;
 
 public class MyFirstProgram {
-	
-	public static void main(String[] args) {
 
-			System.out.println("Moj pierwszy program");
+    public static void main(String[] args) {
 
-		}
+        System.out.println("Moj pierwszy program");
+
+    }
 }
