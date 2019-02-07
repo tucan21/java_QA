ff822138a7cbcc465a5d8580b2d8fdeaa457ff68
tucan21 <stat21@interia.pl>
2019-02-07 21:09:38 +0100

    Zmiana przeglądarki

--- addressbook-web-tests/src/test/java/pl/java_QA/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/test/TestBase.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
