8fc098f10437ad11361cca71fcae136cfdd3886b
tucan21 <stat21@interia.pl>
2019-02-13 22:11:46 +0100

    comparison of collection
    deletes tests

--- addressbook-web-tests/src/test/java/pl/java_QA/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package pl.java_QA.addressbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
   private final String firstname;
   private final String lastname;
@@ -7,6 +9,34 @@ public class ContactData {
   private final String mobile;
   private final String email;
   private String group;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return Objects.equals(firstname, that.firstname) &&
+            Objects.equals(lastname, that.lastname) &&
+            Objects.equals(address, that.address) &&
+            Objects.equals(mobile, that.mobile) &&
+            Objects.equals(email, that.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstname, lastname, address, mobile, email);
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            ", address='" + address + '\'' +
+            ", mobile='" + mobile + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+  }
 
   public ContactData(String firstname, String lastname, String address, String mobile, String email, String group) {
     this.firstname = firstname;
