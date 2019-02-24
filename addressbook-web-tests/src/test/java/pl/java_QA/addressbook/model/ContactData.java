d6b86d36fef4747d435bb758f1994d8c8d422876
tucan21 <stat21@interia.pl>
2019-02-24 18:16:18 +0100

    groups json

--- addressbook-web-tests/src/test/java/pl/java_QA/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/model/ContactData.java
@@ -1,5 +1,6 @@
 package pl.java_QA.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -11,19 +12,33 @@ import java.util.Objects;
 public class ContactData {
   @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+  @Expose
   private String firstname;
+  @Expose
   private String lastname;
+  @Expose
   private String address;
+  @Expose
   private String address2;
+  @Expose
   private String mobilePhone;
+  @Expose
   private String workPhone;
+  @Expose
   private String homePhone;
+  @Expose
   private String allPhones;
+  @Expose
   private String email;
+  @Expose
   private String email2;
+  @Expose
   private String email3;
+  @Expose
   private String group;
+  @Expose
   private String allEmails;
+  @Expose
   private String allAddresses;
   private String allView;
   private File photo;
--- addressbook-web-tests/src/test/java/pl/java_QA/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/test/GroupCreationTests.java
@@ -1,5 +1,7 @@
 package pl.java_QA.addressbook.test;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -31,6 +33,20 @@ public class GroupCreationTests extends TestBase {
     XStream xstream = new XStream();
     xstream.processAnnotations(GroupData.class);
     List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+   Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
     return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
