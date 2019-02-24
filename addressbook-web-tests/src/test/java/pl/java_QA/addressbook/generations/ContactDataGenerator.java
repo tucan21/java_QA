b33ebd5d97a573f6da3186d82b3632385c2ac1b2
tucan21 <stat21@interia.pl>
2019-02-24 18:55:39 +0100

    Contacts json/csv/xml

---
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/generations/ContactDataGenerator.java
@@ -0,0 +1,91 @@
+package pl.java_QA.addressbook.generations;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import pl.java_QA.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    if (format.equals("csv")) {
+      saveAsCSV(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(contacts, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(contacts, new File(file));
+    } else
+      System.out.println("Unrecognized format " + format);
+  }
+
+  private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    String xml = xstream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (ContactData Contact : contacts) {
+      writer.write(String.format("%s;%s;%s\n", Contact.getFirstname(), Contact.getLastname(), Contact.getAddress(),
+              Contact.getEmail()));
+    }
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withFirstname(String.format("firstname %s", i))
+              .withtLastname(String.format("lastname %s", i)).withAddress(String.format("address %s", i))
+              .withAddress2(String.format("address2 %s", i)).withMobilePhone(String.format("+4860245231"))
+              .withWorkPhone(String.format("+48(22)2564152")).withHomePhone(String.format("+48 12 521 96 41"))
+              .withEmail(String.format("email1@email.com")).withGroup(String.format("test1Group"))
+              .withEmail2(String.format("email2@email.com")).withEmail3(String.format("email3@email.com")));
+    }
+    return contacts;
+  }
+}
--- addressbook-web-tests/src/test/java/pl/java_QA/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/test/GroupCreationTests.java
@@ -50,7 +50,7 @@ public class GroupCreationTests extends TestBase {
     return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test(dataProvider = "validGroups")
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
