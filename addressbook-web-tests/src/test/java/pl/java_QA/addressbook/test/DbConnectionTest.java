1731e0ea3b428b48853b46319ee3da410c513183
tucan21 <stat21@interia.pl>
2019-03-01 22:17:12 +0100

    ORM Groups

---
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/test/DbConnectionTest.java
@@ -0,0 +1,39 @@
+package pl.java_QA.addressbook.test;
+import java.sql.*;
+
+import org.testng.annotations.Test;
+import pl.java_QA.addressbook.model.GroupData;
+import pl.java_QA.addressbook.model.Groups;
+
+public class DbConnectionTest {
+
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=" +
+              "&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+      Groups groups = new Groups();
+      while ((rs.next())) {
+        groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+      }
+
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(groups);
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+
+  }
+
+}
