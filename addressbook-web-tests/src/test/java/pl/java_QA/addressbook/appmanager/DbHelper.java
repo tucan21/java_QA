cb4f374f01785ec9e1a4db44bbd6b432caebc762
tucan21 <stat21@interia.pl>
2019-03-05 09:50:54 +0100

    Task nr 12 - Corrected

--- addressbook-web-tests/src/test/java/pl/java_QA/addressbook/appmanager/DbHelper.java
+++ addressbook-web-tests/src/test/java/pl/java_QA/addressbook/appmanager/DbHelper.java
@@ -28,7 +28,8 @@ public class DbHelper {
   public Groups groups() {
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    List<GroupData> result = session.createQuery( "from GroupData" ).list();
+    List<GroupData> result = session.createQuery( "from GroupData where deprecated = '0000-00-00'" )
+            .list();
     session.getTransaction().commit();
     session.close();
     return new Groups(result);
@@ -38,7 +39,8 @@ public class DbHelper {
   public Contacts contacts() {
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    List<ContactData> result = session.createQuery( "from ContactData" ).list();
+    List<ContactData> result = session.createQuery( "from ContactData where deprecated = '0000-00-00'" )
+            .list();
     session.getTransaction().commit();
     session.close();
     return new Contacts(result);
