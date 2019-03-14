1d3ce51c12126922ff0041fb74d0ff3c740f5b5d
tucan21 <stat21@interia.pl>
2019-03-14 23:35:50 +0100

    Lazy initialization of helpers

---
+++ mantis-tests/src/test/java/pl/java_QA/mantis/tests/RegistrationTests.java
@@ -0,0 +1,12 @@
+package pl.java_QA.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+  @Test
+  public void testRegistration() {
+    app.registration().start("user1", "user1@localhost.localdomain");
+
+  }
+}
